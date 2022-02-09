c8a988aa6315ac44404a18537bc8e3604e2f07b7
nnxv <73683440+nnxv@users.noreply.github.com>
2022-02-09 16:31:49 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+jsApiConfig([
+    
+         "http://jx.quanmingjiexi.com/?url=",
+	   "http://max.vxac.cn/svipjx/?url=",
+       "https://jx.xiaolangyun.com/?url=",
+       "https://www.qianyicp.com/jiexi/index.php?url=",
+       "https://jsap.attakids.com/?url=",
+       "http://www.2ajx.com/vip.php?url=",
+
+
+]);
